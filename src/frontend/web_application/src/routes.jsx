67ae2c34155b63a1bf969aee011b5f4b009b5306
David Epely <esion99@gmail.com>
2016-11-23 13:48:37 +0100

    install and configure gandi/react-translate

--- src/frontend/web_application/src/routes.jsx
+++ src/frontend/web_application/src/routes.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
+import { provideTranslate, createTranslator } from '@gandi/react-translate';
 import Page from './components/Page';
 import Discussions from './scenes/Discussions';
 import ContactList from './scenes/ContactList';
 
+const translatorParams = {
+  translations: {       // catalog
+    'This is a test %(username)s!': 'C\'est un test %(username)s!',
+    'call-to-action.action.compose': 'Compose',
+    'call-to-action.action.create_contact': 'Contact',
+  },
+  locale: 'fr',         // user's locale
+  utcOffset: 0,         // user's zone offset
+  defaultLocale: 'en',  // default application locale
+  logMissing: false,    // display warnings when translations are missing (except on production)
+  localeData: {         // Config for TODO
+    locale: 'fr',
+    number: {
+      currencies: {
+        USD: '$US',
+        EUR: '€',
+      },
+    },
+  },
+};
+
+const translator = createTranslator(translatorParams);
+const App = provideTranslate(translator)(Page);
+
 const routes = (
-  <Route path="/" component={Page} >
+  <Route name="app" path="/" component={App} >
     <IndexRoute component={Discussions} />
     <Route path="contacts" component={ContactList} />
   </Route>
 );
+
 
 export default routes;
