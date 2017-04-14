712e673cc4bc79d97de2141f6e9250b1b9db06ff
Stan <git-ssa@sapienssapide.com>
2017-04-14 19:45:16 +0200

    Add consistency validation on routes POST/PATCH message draft
    if draft has a discussion_id or a parent_id, provided draft's params are checked against the discussion attributes, notably :
    *participants
    *subject
    
    otherwise a discussion_id is added to the draft.

--- src/backend/main/py.main/caliopen_main/objects/message.py
+++ src/backend/main/py.main/caliopen_main/objects/message.py
@@ -9,6 +9,7 @@ import uuid
 from uuid import UUID
 import datetime
 import json
+import re
 
 from caliopen_main.message.store import Message as ModelMessage
 from caliopen_main.message.store import IndexedMessage
@@ -18,10 +19,14 @@ from caliopen_main.message.core import RawMessage
 from .tag import ResourceTag
 from .attachment import MessageAttachment
 from .external_references import ExternalReferences
-from .identities import Identity
+from .identities import Identity, LocalIdentity
+from caliopen_main.user.core import User
 from .participant import Participant
 from .privacy_features import PrivacyFeatures
-
+import caliopen_main.errors as err
+from caliopen_storage.exception import NotFound
+from caliopen_main.discussion.store.discussion_index import \
+    DiscussionIndexManager as DIM
 
 import logging
 
@@ -86,20 +91,26 @@ class Message(base.ObjectIndexable):
 
         :params: a NewMessage dict
         """
+
         if user_id is None or user_id is "":
             raise ValueError
 
         try:
             message_param = NewMessage(params)
             message_param.validate()
+            valid_draft = Message.validate_draft_consistency(user_id,
+                                                             message_param,
+                                                             True)
         except Exception as exc:
+            log.warn(exc)
             raise exc
+
         message = Message()
-        message.unmarshall_json_dict(params)
+        message.unmarshall_json_dict(valid_draft)
         message.user_id = UUID(user_id)
         message.message_id = uuid.uuid4()
         message.is_draft = True
-        message.type = "email"  # TODO: type handling infered from participants
+        message.type = "email"  # TODO: type handling inferred from participants
         message.date_insert = datetime.datetime.utcnow()
 
         try:
@@ -115,6 +126,150 @@ class Message(base.ObjectIndexable):
             log.warn(exc)
             raise exc
         return message
+
+    def patch_draft(self, patch, **options):
+        """operations specific to draft, before applying generic patch method"""
+
+        self.get_db()
+        self.unmarshall_db()
+        if not self.is_draft:
+            return err.PatchUnprocessable(message="this message is not a draft")
+
+        try:
+            params = dict(patch)
+            params.pop("current_state")
+            message_param = NewMessage(params)
+            message_param.validate()
+            valid_draft = Message.validate_draft_consistency(self.user_id,
+                                                             message_param,
+                                                             False)
+            print(valid_draft)
+        except Exception as exc:
+            log.warn(exc)
+            return err.PatchError(message=exc.message)
+
+        self.apply_patch(patch, **options)
+
+    @classmethod
+    def validate_draft_consistency(cls, user_id, draft, is_new):
+        """
+        Function used by create_draft and patch_draft
+        to unsure provided params are consistent with draft's context
+        
+        :param user_id as a string
+        :param draft is a dict conforming to Message param models
+        :param is_new : if true indicates that we want to validate a new draft,
+                                    otherwise it is an update of existing one
+                            
+        If needed, draft is modified to conform.
+        Returns a validated draft or error.
+        """
+        # remove 'from' participant
+        if 'participants' in draft and len(draft['participants']) > 0:
+            participants = draft['participants']
+            for i, participant in enumerate(participants):
+                if re.match("[Ff][Rr][Oo][Mm]", participant['type']):
+                    participants.pop(i)
+
+        if is_new:
+            if 'identities' not in draft:
+                raise err.PatchUnprocessable
+
+            if len(draft['identities']) != 1:
+                raise err.PatchUnprocessable
+
+            provided_identity = draft['identities'][0]
+            local_identity = LocalIdentity(
+                identifier=provided_identity['identifier'])
+            try:
+                local_identity.get_db()
+                local_identity.unmarshall_db()
+            except NotFound:
+                raise NotFound
+            if str(local_identity.user_id) != user_id:
+                raise err.ForbiddenAction
+
+            # add 'from' participant with local identity's identifier
+            user = User.get(user_id)
+            if 'participants' not in draft:
+                draft['participants'] = []
+            participants = draft['participants']
+            from_participant = Participant()
+            from_participant.address = local_identity.identifier
+            from_participant.label = local_identity.display_name
+            from_participant.protocol = "email"
+            from_participant.type = "From"
+            from_participant.contact_ids = [user.contact.contact_id]
+            participants.append(from_participant.marshall_dict())
+
+        # discussion consistency
+        new_discussion = False
+        if 'discussion_id' not in draft or draft['discussion_id'] == "" \
+                or draft['discussion_id'] is None:
+            # no discussion_id provided. Try to find one with draft's parent_id
+            # or create new discussion
+            if 'parent_id' in draft and draft['parent_id'] != "" \
+                    and draft['parent_id'] is not None:
+                parent_msg = Message(user_id, message_id=draft['parent_id'])
+                try:
+                    parent_msg.get_db()
+                    parent_msg.unmarshall_db()
+                except NotFound:
+                    raise err.PatchError(message="parent message not found")
+                draft['discussion_id'] = parent_msg.discussion_id
+            else:
+                draft['discussion_id'] = uuid.uuid4()
+                new_discussion = True
+
+        if not new_discussion:
+            dim = DIM(user.user_id)
+            d_id = draft['discussion_id']
+            last_message = dim.get_last_message(d_id, 0, 100)
+            if last_message == {}:
+                raise err.PatchError(
+                    message='No such discussion {}'.format(d_id))
+
+            # check participants consistency
+            if 'participants' in draft and len(draft['participants']) > 0:
+                participants = [p['address'] for p in draft['participants']]
+                last_msg_participants = [p['address'] for p in
+                                         last_message['participants']]
+                if len(participants) != len(last_msg_participants):
+                    raise err.PatchError(
+                        message="list of participants "
+                                "is not consistent for this discussion")
+                participants.sort()
+                last_msg_participants.sort()
+                for i, participant in enumerate(participants):
+                    if participant != last_msg_participants[i]:
+                        raise err.PatchConflict(
+                            message="list of participants "
+                                    "is not consistent for this discussion")
+            else:
+                # no participant provided : should not happen within a reply
+                raise err.PatchError(
+                    message="No participants provided for this reply")
+
+            # check parent_id consistency
+            if 'parent_id' in draft and draft['parent_id'] != "" \
+                    and draft['parent_id'] is not None:
+                if not dim.get_a_message_id(draft['discussion_id'],
+                                            draft['parent_id']):
+                    raise err.PatchConflict(message="provided message "
+                                                    "parent_id does not belong"
+                                                    "to this discussion")
+
+            # check subject consistency
+            if 'subject' in draft:
+                if draft['subject'] != last_message['subject']:
+                    raise err.PatchConflict(message="subject has been changed")
+            else:
+                draft['subject'] = last_message['subject']
+
+        # prevent modification of protected attributes
+        # TODO: below attributes should be protected by Message class
+
+        return draft
 
     @classmethod
     def by_discussion_id(cls, user, discussion_id, min_pi, max_pi,
