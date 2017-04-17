bcdd3ffb8da4b147fe0b88ee8cc453765e8e1603
Aymeric Barantal <mric@gandi.net>
2017-04-17 11:18:48 +0200

    Variable referenced before usage, void imports making recursive import and a typo

--- src/backend/main/py.main/caliopen_main/message/store/__init__.py
+++ src/backend/main/py.main/caliopen_main/message/store/__init__.py
@@ -3,12 +3,6 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from .attachment import MessageAttachment
 from .attachment_index import IndexedMessageAttachment
-from caliopen_main.discussion.store.discussion import (Discussion,
-                                                       DiscussionExternalLookup,
-                                                       DiscussionMessageLookup,
-                                                       DiscussionRecipientLookup)
-from caliopen_main.discussion.store.discussion_index import \
-    DiscussionIndexManager
 from .external_references import ExternalReferences
 from .external_references_index import IndexedExternalReferences
 from .message import Message
@@ -20,10 +14,6 @@ from .raw import RawMessage, UserRawLookup
 __all__ = ['MessageAttachment', 'IndexedMessageAttachment',
            'RawMessage', 'UserRawLookup',
            'Message', 'IndexedMessage',
-           'DiscussionIndexManager'
-           'Discussion', 'DiscussionMessageLookup',
-           'DiscussionRecipientLookup', 'DiscussionExternalLookup',
-           'DiscussionIndexManager',
            'ExternalReferences', 'IndexedExternalReferences',
            'Participant', 'IndexedParticipant'
            ]
