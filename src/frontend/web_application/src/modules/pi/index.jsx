8b3c38dd5823af4b34265ea2f54a946eeace9341
David Epely <esion99@gmail.com>
2023-04-03 21:09:22 +0200

    [frontend] add PiScore component

--- src/frontend/web_application/src/modules/pi/index.jsx
+++ src/frontend/web_application/src/modules/pi/index.jsx
@@ -1,3 +1,4 @@
 export { default as MultidimensionalPi } from './components/MultidimensionalPi';
 export { default as BackgroundImage } from './components/BackgroundImage';
+export { default as PiScore } from './components/PiScore';
 export * from './services/pi';
