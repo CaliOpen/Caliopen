0f2fbbdfd54aac4024f8327a6a8dcd27792ffe8f
David Epely <esion99@gmail.com>
2023-11-29 07:48:42 +0100

    [devtools] migrate deprecated swagger cli to recommanded '@redocly/cli'

--- devtools/gen-swagger-spec.sh
+++ devtools/gen-swagger-spec.sh
@@ -7,19 +7,8 @@ PROJECT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 DEST_DIR=${PROJECT_DIRECTORY}/src/backend/configs
 SRC_DIR=${PROJECT_DIRECTORY}/src/backend/defs/rest-api
-SWAGGER_CLI_DIR=${PROJECT_DIRECTORY}/devtools/swagger-cli
-SWAGGER_JS=${SWAGGER_CLI_DIR}/node_modules/.bin/swagger
-CMD="yarn start -- bundle -r ${SRC_DIR}/swagger-root.yaml -o ${DEST_DIR}/swagger.json"
+CMD="npx @redocly/cli bundle ${SRC_DIR}/swagger-root.yaml -o ${DEST_DIR}/swagger.json"
 
-
-if [[ ! -f "${SWAGGER_JS}" ]]; then
-	echo "You should have nodejs > 6 and yarn"
-	echo "Run npm install swagger in ${DEST_DIR} directory"
-
-	(cd $SWAGGER_CLI_DIR && yarn install)
-fi
-
-
-(cd $SWAGGER_CLI_DIR && ${CMD})
+${CMD}
 
 set +e
